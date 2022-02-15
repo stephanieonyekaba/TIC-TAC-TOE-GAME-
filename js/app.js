2f2f52c7651c51687402e2fcdd96ce588c7d99e6
stephanieonyekaba <onyek009@umn.edu>
2022-02-14 20:01:12 -0500

    Tic tac toe assignment

---
+++ js/app.js
@@ -0,0 +1,45 @@
+const board;
+const player1 = "x";
+const player2 = "o";
+let currentPlayer = "player2";
+let gameOver = false;
+
+window.onload = function () => {
+    startGame();
+    }
+
+    
+
+
+
+
+
+           
+
+            boardItem.addEventListener('click', setItem);
+        }
+
+        function setItem() {
+            if(gameOver) {
+                return;
+            }
+        }
+        
+    }
+
+}
+
+
+
+
+
+// function setBoardItem() {
+
+
+
+
+
+
+
+//         }
+//     }
